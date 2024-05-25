84f3062d6b56e5d6b267d7689d469d8cb57d840d
rishireddy2000 <rishireddy@uchicago.edu>
2024-05-25 17:08:35 -0500

    final base

--- QuickRecipe/QuickRecipe/RecipeSearchView.swift
+++ QuickRecipe/QuickRecipe/RecipeSearchView.swift
@@ -8,10 +8,16 @@ struct Recipe: Codable, Identifiable {
     var usedIngredientCount: Int?
     var missedIngredientCount: Int?
     var likes: Int?
+    var missedIngredients : [Ingredient]?
 
     enum CodingKeys: String, CodingKey {
-        case id, title, image, summary, usedIngredientCount, missedIngredientCount, likes
+        case id, title, image, summary, usedIngredientCount, missedIngredientCount, likes, missedIngredients
     }
+}
+
+struct RecipeDetail: Codable {
+    var sourceUrl: String
+    var instructions: String
 }
 
 struct Ingredient: Codable {
@@ -36,17 +42,19 @@ struct RecipeRow: View {
                 }
             }
             if showDetails {
-                Text(recipe.summary ?? "")
-                    .font(.caption)
-                    .foregroundColor(.gray)
+                if let missedIngredients = recipe.missedIngredients {
+                    Text("Missing Ingredients: \(missedIngredients.map { $0.name }.joined(separator: ", "))")
+                } else {
+                    Text("Missing Ingredients: None")
+                }
             }
         }
     }
 }
 
+
 struct RecipeSearchView: View {
-    @State private var recipes = [Recipe(id: 1, title: "Test Recipe", image: "", summary: "Just a test")]
-//    @State private var recipes = [Recipe]()
+    @State private var recipes = [Recipe]()
     @State private var isFetching = false
     var ingredients: [String]  // This should be passed from the main view
 
@@ -70,7 +78,6 @@ struct RecipeSearchView: View {
     }
 
     func fetchRecipes() {
-        print("1")
         let ingredientQuery = ingredients.joined(separator: ",")
         guard let url = URL(string: "https://api.spoonacular.com/recipes/findByIngredients?ingredients=\(ingredientQuery)&apiKey=5c1d49c5ed4948bca4af1050b57da95d") else { return }
         isFetching = true
@@ -89,13 +96,12 @@ struct RecipeSearchView: View {
                     print("No data received")
                     return
                 }
-                print("Found recipes, raw data: \(String(describing: String(data: data, encoding: .utf8)))")
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 decoder.dateDecodingStrategy = .iso8601
                 do {
                     let decodedResponse = try decoder.decode([Recipe].self, from: data)
-                    recipes = decodedResponse
+                    recipes = decodedResponse.sorted { $0.missedIngredientCount ?? 0 < $1.missedIngredientCount ?? 0 }
                 } catch {
                     print("Failed to decode JSON: \(error)")
                 }
